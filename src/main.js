ffb3df0c58fbfcdc331e2cc731f3abf3cdf0db09
VIVIFY <vivify@MacBook-Pro.local>
2020-01-24 14:16:49 +0100

    #IMDD-6 Setup axios, router, vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import {store} from './store/store';
+import axios from './vue-axios';
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history'
+})
 
 new Vue({
+  router,
+  axios,
+  store,
   render: h => h(App),
 }).$mount('#app')
